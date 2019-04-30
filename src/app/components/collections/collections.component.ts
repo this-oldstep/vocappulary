8acaf994d598810a5ca8ede8a35963896df07686
Patrick Ryan <patrickry07@gmail.com>
2019-04-30 18:39:13 -0500

    collections component working

---
+++ src/app/components/collections/collections.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: 'collections',
+  templateUrl: './collections.component.html',
+  styleUrls: ['/collections.component.css'],
+  moduleId: module.id
+})
+export class Collections {} 
