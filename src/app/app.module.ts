75938b0db67e74da0919664a16c918014007b044
Arnulfo <chango88@protonmail.com>
2019-05-03 17:04:06 -0500

    work on collection input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-
+import {NativeScriptFormsModule} from 'nativescript-angular/forms'
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 // import { Collections } from "./components/collections/collections.component"
+import { PracticaComponent } from './components/practica/practica.component'
+import {CollectionsComponent} from './components/collections/collections.component';
 
 @NgModule({
     bootstrap: [
@@ -11,13 +13,14 @@ import { AppComponent } from "./app.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptFormsModule
     ],
     declarations: [
         AppComponent,
         // Collections// hello
-
-
+        PracticaComponent,
+        CollectionsComponent,
     ],
     schemas: [
         NO_ERRORS_SCHEMA
