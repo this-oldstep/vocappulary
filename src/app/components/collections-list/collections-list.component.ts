72095704d1c675e67b2e4cb15049e7f022b1ff75
Patrick Ryan <patrickry07@gmail.com>
2019-05-03 15:08:59 -0500

    base64 working

--- src/app/components/collections-list/collections-list.component.ts
+++ src/app/components/collections-list/collections-list.component.ts
@@ -1,6 +1,18 @@
 import { Component } from "@angular/core"
 import { HttpClient } from "@angular/common/http"
 import * as camera from "nativescript-camera";
+import bgHttpModule = require('nativescript-background-http')
+import { NgModule } from '@angular/core';
+
+const imageSourceModule = require("tns-core-modules/image-source");
+let imagePicker = require("nativescript-imagepicker")
+let fs = require("file-system")
+
+const fsModule = require("tns-core-modules/file-system");
+import enumsModule = require('tns-core-modules/ui/enums')
+import { fromAsset } from "tns-core-modules/image-source/image-source";
+
+
 @Component({
   selector: 'collections-list',
   templateUrl: './collections-list.component.html',
@@ -14,18 +26,53 @@ export class CollectionsList {
   constructor(private http: HttpClient) {
 
   }
+  public myImage=""
+
+  getPicture(){
+    console.log("getpicture")
+   let context = imagePicker.create({
+      mode: "single"
+    });
+    context
+      .authorize()
+      .then(function () {
+        return context.present();
+      })
+      .then(function (selection) {
+        selection.forEach(function (selected) {
+          // console.log(selected)
+          console.log(selected.u)
+        });
+        // list.items = selection;
+      }).catch(function (e) {
+        // process error
+      });
+
+  }
+
+
+
+
+
+
   uploadPhoto() {
     camera.requestPermissions().then(
       function success() {
         console.log('yes')
         let options = { width: 100, height: 100, keepAspectRatio: false, saveToGallery: true };
         let imageModule = require("tns-core-modules/ui/image");
+        const format = enumsModule.ImageFormat.jpeg
+
         camera.takePicture(options)
           .then(function (imageAsset) {
             console.log("Result is an image asset instance");
             var image = new imageModule.Image();
             image.src = imageAsset;
             console.log(imageAsset.options.width, imageAsset.options.height)
+            fromAsset(imageAsset).then((result)=>{
+              let base64 = result.toBase64String("jpeg", 100);
+              console.log(base64);
+            })
           }).catch(function (err) {
             console.log("Error -> " + err.message);
           });
