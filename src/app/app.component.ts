be0de9b420dfb306fdb05cb936d5ee31cd7096a6
Quinn McCourt <quinn.mccourt@gmail.com>
2019-05-04 11:41:27 -0500

    Added side drawer and action bar icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,47 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy, ViewChild, AfterViewInit, ChangeDetectorRef } from "@angular/core";
+import { UIService } from "./shared/ui.serivce";
+import { Subscription } from "rxjs";
+import { RadSideDrawerComponent } from "nativescript-ui-sidedrawer/angular/side-drawer-directives";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 
 @Component({
     moduleId: module.id,
     selector: "ns-app",
     templateUrl: "app.component.html"
 })
-export class AppComponent {
-    
-    //enteredCollection="";
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
+    @ViewChild(RadSideDrawerComponent) drawerComponent: RadSideDrawerComponent
+    private drawerSub: Subscription
+    private drawer: RadSideDrawer;
+    constructor(private uiService: UIService, private changeDetectionRef: ChangeDetectorRef) {
+
+    }
+
+    ngOnInit() {
+        this.drawerSub = this.uiService.drawerState.subscribe( () => {
+            if (this.drawer) {
+                this.drawer.toggleDrawerState();
+            }
+        });
+    }
     activeCollections: string[] = [];
 
+    ngAfterViewInit() {
+        this.drawer = this.drawerComponent.sideDrawer;
+        this.changeDetectionRef.detectChanges();
+    }
 
     onCollectionInput(collectionDescription: string) {
         this.activeCollections.push(collectionDescription);
     }
 
+    onLogout() {
+        this.uiService.toggleDrawer();
+    }
+
+    ngOnDestroy() {
+        if (this.drawerSub) {
+            this.drawerSub.unsubscribe();
+        }
+    }
  }
