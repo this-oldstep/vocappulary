3776db2ba2129f13dc25df9fddbf1e989c6c89a8
Quinn McCourt <quinn.mccourt@gmail.com>
2019-05-04 16:21:15 -0500

    Creating user object

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { alert } from 'tns-core-modules/ui/dialogs'
+import { User } from './user.model';
 
 const FIREBASE_API_KEY = 'AIzaSyC8WpwuQBkU6xoVHnoZ59xyDk9pdvFNeR0'
+
+interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+}
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
     constructor(private http: HttpClient) {}
 
     signUp(email: string, password: string) {
-       return this.http.post(
+       return this.http.post<AuthResponseData>(
             `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${FIREBASE_API_KEY}`
             , {email: email, password: password, returnSecureToken: true}
             ).pipe(catchError(errorRes => {
                 this.handleError(errorRes.error.error.message)
                 return throwError(errorRes);
-            }));
+            }),
+            tap(resData => {
+                if (resData && resData.idToken) {
+                    const expirationDate = new Date(new Date().getTime() + parseInt(resData.expiresIn) * 1000);
+                    const user = new User(email, resData.localId, resData.idToken, expirationDate)
+                }
+            })
+            );
         }
     login(email: string, password: string) {
-       return this.http.post(
+       return this.http.post<AuthResponseData>(
             `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${FIREBASE_API_KEY}`
             , {email: email, password: password, returnSecureToken: true}
             ).pipe(catchError(errorRes => {
---
+++ src/app/auth/user.model.ts
@@ -0,0 +1,8 @@
+export class User {
+    constructor(public email: string, public id: string, private _token: string, private _tokenExpirationDate: Date 
+        ) {}
+
+        get token() {
+            return this._token;
+        }
+}
