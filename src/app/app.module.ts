8bcd6842d7bc60b8633b921881626b88195844a0
Quinn McCourt <quinn.mccourt@gmail.com>
2019-05-04 09:39:29 -0500

    Added sharable action bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import {NativeScriptFormsModule} from 'nativescript-angular/forms'
+import { NativeScriptFormsModule } from 'nativescript-angular/forms'
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AuthComponent } from './auth/auth.component';
@@ -9,7 +9,8 @@ import {CollectionsComponent} from './components/collections/collections.compone
 import { CollectionInputComponent } from './components/collection-input/collection-input.component';
 import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular/side-drawer-directives'
 import { HttpClientModule } from '@angular/common/http';
-import { LandingComponent } from './components/landing/landing.component'
+import { LandingComponent } from './components/landing/landing.component';
+import { ActionBarComponent } from './shared/ui/action-bar/action-bar.component'
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { LandingComponent } from './components/landing/landing.component'
         CollectionsComponent,
         CollectionInputComponent,
         AuthComponent,
-        LandingComponent
+        LandingComponent,
+        ActionBarComponent
     ],
     schemas: [
         NO_ERRORS_SCHEMA
