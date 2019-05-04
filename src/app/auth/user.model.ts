9f03edf1806c11e0d37040c1f93102e5aaff9e42
Quinn McCourt <quinn.mccourt@gmail.com>
2019-05-04 16:58:34 -0500

    update user model

--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -1,5 +1,10 @@
 export class User {
-    constructor(public email: string, public id: string, private _token: string, private _tokenExpirationDate: Date 
+    constructor(public email: string, 
+        public languageNative: string, 
+        public langaugeCurrent: string, 
+        public id: string, 
+        private _token: string, 
+        private _tokenExpirationDate: Date 
         ) {}
 
         get token() {
