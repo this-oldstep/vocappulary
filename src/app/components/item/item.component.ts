46134d16ebb058db0847c957f573dc8f3c6353db
amanriquez <amanriquez.guitar@gmail.com>
2019-05-07 22:19:13 -0500

    succesfully playing audio

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { TNSPlayer } from 'nativescript-audio';
 
 @Component({
   selector: 'ns-item',
@@ -11,16 +11,43 @@ import { ActivatedRoute } from '@angular/router';
 export class ItemComponent implements OnInit {
 
   public item: any;
+  private _player: TNSPlayer;
 
   constructor(private activatedRoute: ActivatedRoute) {
     this.activatedRoute.queryParams.subscribe( params => {
       this.item = params;
     })
+    this._player = new TNSPlayer;
+    this._player.debug = true;
+
    }
 
   ngOnInit() {
 
     console.log('info coming to item component', this.item)
   }
+
+  playWord(){
+
+    console.log('playing audio file');
+    const playerOptions = {
+      audioFile: this.item.audio_url,
+      loop: false,
+      completeCallback: function () {
+        console.log('finished playing');
+      },
+      errorCallback: function (errorObject) {
+        console.log(JSON.stringify(errorObject));
+      },
+      infoCallback: function (args) {
+        console.log(JSON.stringify(args));
+      }
+    };
+
+    this._player.playFromUrl(playerOptions);
+
+  }
+
+
 
 }
