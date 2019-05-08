f31ed72620f5bd03fb02f082e3a0b0e610474895
amanriquez <amanriquez.guitar@gmail.com>
2019-05-07 19:26:19 -0500

    collections on two column grid layout and cyan comp color

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,8 +3,6 @@ import { NativeScriptCommonModule } from "nativescript-angular/common";
 import { HttpClientModule } from '@angular/common/http';
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
-//import { Collections } from "../components/collections/collections.component";
-import { CollectionsList } from "../components/collections-list/collections-list.component";
 import { Buddies } from "../components/buddies/buddies.component";
 import { Login } from "../components/login/login.component";
 import { PhotoComponentComponent } from "../components/photo-component/photo-component.component"
@@ -18,7 +16,6 @@ import { PhotoComponentComponent } from "../components/photo-component/photo-com
     declarations: [
         PhotoComponentComponent,
         HomeComponent,
-        CollectionsList,
         Buddies,
         Login
     ],
