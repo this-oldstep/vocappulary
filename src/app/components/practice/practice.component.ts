b5fdcb1a7d706283a1ca7fdc04bbca41c649d6e9
amanriquez <amanriquez.guitar@gmail.com>
2019-05-09 14:45:19 -0500

    recording tests

--- src/app/components/practice/practice.component.ts
+++ src/app/components/practice/practice.component.ts
@@ -4,7 +4,11 @@ import { HttpClient } from "@angular/common/http";
 import { NGROK } from '../../../config'
 import { TNSPlayer, TNSRecorder } from 'nativescript-audio';
 import { knownFolders, Folder, File } from "tns-core-modules/file-system";
+import { hasPermission, requestPermission, requestPermissions} from 'nativescript-permissions';
+const permissions = require('nativescript-permissions');
 
+
+declare var android: any;
 
 
 @Component({
@@ -13,11 +17,15 @@ import { knownFolders, Folder, File } from "tns-core-modules/file-system";
   templateUrl: "./practice.component.html",
 })
 export class PracticeComponent implements OnInit {
+  
   public cards: any;
+  private _recorder: TNSRecorder;
 
 
   constructor(private http: HttpClient) {
   
+    this._recorder = new TNSRecorder;
+
   }
 
 
@@ -35,6 +43,62 @@ export class PracticeComponent implements OnInit {
 
   onRecord(){
 
+    /*
+    add functionality for is recording.
+    */
+    permissions.requestPermission(android.Manifest.permission.RECORD_AUDIO, "Say the word!...")
+      .then(function (){
+          if (TNSRecorder.CAN_RECORD()){
+            console.log('I can record!');
+
+            let audioFolder = knownFolders.currentApp().getFolder("audio");
+
+            let recorderOptions = {
+              filename: audioFolder.path + '/macarena.mp3',
+              infoCallback: function () {
+                console.log('infoCallback');
+              },
+              errorCallback: function () {
+                console.log('errorCallback');
+                alert('Error recording.');
+              }
+            };
+
+
+            this._recorder.start(recorderOptions)
+              .then((result) => {
+                console.log('recording!!!!')
+              }).catch((err) => {
+                console.log('not recording :(')
+              });
+
+
+          } else{
+            alert('device cannot recourd audio');
+          }
+      })
+      .catch(err => {
+        console.log('no permissions can\'t record');
+      })
+
   }
+
+  stopRecording(){
+
+
+    if (this._recorder !== undefined){
+      this._recorder.stop()
+        .then((result) => {
+          console.log('stopped recording!')
+        }).catch((err) => {
+          console.log('oh no can\'t stop recording!');
+        });
+    }
+
+
+  }
+
+
+
 
 }
